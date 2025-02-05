60fad1e38d4cf196e75b798b54ac15fb412d87ee
PedroH <pedro2266809ph@gmail.com>
2025-02-04 22:44:25 -0300

    feat: add method findAll pageable

--- src/main/java/com/ph/ecommerce/controllers/ProductController.java
+++ src/main/java/com/ph/ecommerce/controllers/ProductController.java
@@ -1,18 +1,18 @@
 package com.ph.ecommerce.controllers;
 
 import com.ph.ecommerce.dto.ProductDTO;
-import com.ph.ecommerce.entities.Product;
-import com.ph.ecommerce.repositories.ProductRepository;
 import com.ph.ecommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.List;
+
 
 @RestController
 @RequestMapping(value = "/product")
@@ -27,4 +27,12 @@ public class ProductController {
 
         return ResponseEntity.ok().body(dto);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findById(Pageable pageable) {
+        Page<ProductDTO> dto = service.findAll(pageable);
+
+        return ResponseEntity.ok().body(dto);
+    }
+
 }
--- src/main/java/com/ph/ecommerce/services/ProductService.java
+++ src/main/java/com/ph/ecommerce/services/ProductService.java
@@ -4,8 +4,11 @@ import com.ph.ecommerce.dto.ProductDTO;
 import com.ph.ecommerce.entities.Product;
 import com.ph.ecommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
@@ -18,5 +21,12 @@ public class ProductService {
         Product product = repository.findById(id).orElseThrow(() -> new RuntimeException("Id not found"));
 
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> product = repository.findAll(pageable);
+
+       return product.map(products -> new ProductDTO(products));
     }
 }

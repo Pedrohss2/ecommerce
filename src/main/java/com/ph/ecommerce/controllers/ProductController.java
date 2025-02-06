774ee869341cc8bcc3a30a8e90771583d2793565
PedroH <pedro2266809ph@gmail.com>
2025-02-05 22:07:39 -0300

    feat: add method create

--- src/main/java/com/ph/ecommerce/controllers/ProductController.java
+++ src/main/java/com/ph/ecommerce/controllers/ProductController.java
@@ -6,12 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -35,4 +30,11 @@ public class ProductController {
         return ResponseEntity.ok().body(dto);
     }
 
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        dto = service.insert(dto);
+
+        return ResponseEntity.ok().build();
+    }
 }
+
--- src/main/java/com/ph/ecommerce/services/ProductService.java
+++ src/main/java/com/ph/ecommerce/services/ProductService.java
@@ -4,11 +4,10 @@ import com.ph.ecommerce.dto.ProductDTO;
 import com.ph.ecommerce.entities.Product;
 import com.ph.ecommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
@@ -27,6 +26,20 @@ public class ProductService {
     public Page<ProductDTO> findAll(Pageable pageable) {
         Page<Product> product = repository.findAll(pageable);
 
-       return product.map(products -> new ProductDTO(products));
+       return product.map(ProductDTO::new);
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product product = new Product();
+
+        product.setName(dto.getName());
+        product.setDescription(dto.getDescription());
+        product.setImgUrl(dto.getImgUrl());
+        product.setPrice(dto.getPrice());
+
+        product = repository.save(product);
+
+        return new ProductDTO(product);
     }
 }
